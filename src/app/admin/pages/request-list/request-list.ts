7438ceb580d11a210559cef021680854720ddc2a
TS <tinusunny04@gmail.com>
2026-02-04 17:30:10 +0530

    admin request-list desing  complted

--- src/app/admin/pages/request-list/request-list.ts
+++ src/app/admin/pages/request-list/request-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AdminSidebar } from "../../components/admin-sidebar/admin-sidebar";
 
 @Component({
   selector: 'app-request-list',
-  imports: [],
+  imports: [AdminSidebar],
   templateUrl: './request-list.html',
   styleUrl: './request-list.css',
 })
