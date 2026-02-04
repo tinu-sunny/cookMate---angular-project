6b15c8a824ddded60f9f81df4e60be55bfbd3972
TS <tinusunny04@gmail.com>
2026-02-04 16:21:26 +0530

    admin dashboard desing complted

--- src/app/admin/pages/admin-dashboard/admin-dashboard.ts
+++ src/app/admin/pages/admin-dashboard/admin-dashboard.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { AdminSidebar } from "../../components/admin-sidebar/admin-sidebar";
+import { Calender } from "../../components/calender/calender";
+import { Graph } from "../../components/graph/graph";
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [AdminSidebar],
+  imports: [AdminSidebar, Calender, Graph],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
